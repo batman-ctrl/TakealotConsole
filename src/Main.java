b3ef80886aca49d2ee77a6b322e4b361cf67a9b7
John Wick <john@gmail.com>
2024-10-10 11:17:07 +0200

    New : added a loop for a user menu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -21,5 +23,79 @@ public class Main {
 
          */
 
+        // Initialize 10 products
+        Product products[] = new Product[10];
+        products[0] = new Product(1,"Dell 3250", 1999.99);
+        products[1] = new Product(2,"Iphone 15", 30000.00);
+        products[2] = new Product(3,"Lays Chips", 15.00);
+        products[3] = new Product(4,"Laser Printer", 5000.00);
+        products[4] = new Product(5,"Hisense Fridge", 8000.00);
+        products[5] = new Product(6,"JVC Smart TV", 4500.00);
+        products[6] = new Product(7,"Samsung OLED Screen", 15000.00);
+        products[7] = new Product(8,"Bike", 2000.00);
+        products[8] = new Product(9,"Keyboard Reddragon", 499.00);
+        products[9] = new Product(10,"Apple Macbook Pro", 300000.00);
+
+        ShoppingCart cart = new ShoppingCart();
+
+        Scanner scanner = new Scanner(System.in);
+        // 0 = exit
+        int choice = 1;
+
+        while(choice!=0){
+            pl("\nWelcome to Takealot Console V1.0");
+            pl("1. View Products");
+            pl("2. Add Product to Cart");
+            pl("3. View Shopping Cart");
+            pl("0: Exit");
+
+            p("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                case 0:
+                    // Exit
+                    pl("Thank you for choosing Takelot! goodbye!");
+                    break;
+                case 1:
+                    // View products
+                    pl("\nProduct Catalog: ");
+                    for(Product product : products){
+                        product.displayProduct();
+                    }
+                    break;
+                case 2:
+                    // Add to cart
+                    p("Enter the Product Id to add to cart: ");
+                    int productId = scanner.nextInt();
+                    boolean found = false;
+                    for (Product product:products){
+                        if(product.getId()==productId){
+                            cart.addProduct(product);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(!found){
+                        pl("Product not found.");
+                    }
+                    break;
+                case 3:
+                    cart.viewcart();
+                    break;
+                default:
+                    pl("Invalid choice. Please try again.");
+            }
+
+        }
+
+    }
+
+    private static void pl(String s){
+        System.out.println(s);
+    }
+
+    private static void p(String s){
+        System.out.print(s);
     }
 }
--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -11,6 +11,19 @@ public class ShoppingCart {
     public void addProduct(Product product){
         cartItems.add(product);
         System.out.println(product.getName()+" has been added to your cart.");
+    }
 
+    public void viewcart(){
+        if(cartItems.isEmpty()){
+            System.out.println("Your cart is empty.");
+        }else{
+            System.out.println("Your shopping cart:");
+            double total = 0;
+            for(Product product : cartItems){
+                product.displayProduct();
+                total+=product.getPrice();
+            }
+            System.out.println("Total : R"+total);
+        }
     }
 }
